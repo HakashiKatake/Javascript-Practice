61d67cb1d7a219fd3260441e31b1fd8cd22e01a7
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-05-27 22:44:31 +0530

    datatypes

---
+++ 01-Basics/02-datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("hello"); //we are using node js , not browser, so no alert
+
+let name = "Saurabh";
+let age = 22;
+let isLoggedIn = true;
+let state = null;
+
+//number -> 2 to power 53
+//bigint -> large integers
+//string => ""
+//boolean => true/false
+//null => empty value
+//undefined => variable not defined
+//symbol => unique
+
+//object
+
+console.log(typeof null);
+console.log(typeof undefined);
