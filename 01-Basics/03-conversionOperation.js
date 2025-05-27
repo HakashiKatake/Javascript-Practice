779b37cc18d956960786c5430a371c9dfc06df1e
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-05-28 00:04:12 +0530

    operators

--- 01-Basics/03-conversionOperation.js
+++ 01-Basics/03-conversionOperation.js
@@ -26,3 +26,37 @@ let stringNumber = String(someNumber);
 console.log(typeof someNumber);
 console.log(typeof stringNumber);
 
+//operations
+
+let value = 3
+let negValue = -value;
+console.log(negValue)
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2/2)
+//console.log(2**2) //exponentiation
+//console.log(2%2) //modulus
+
+let str1 = "hello";
+let str2 = "world";
+
+let str3 = str1  + str2;
+console.log(str3); //concatenation
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+false);
+console.log(+"abc"); //NaN
+console.log(+""); 
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+
