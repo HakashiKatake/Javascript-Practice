ef865f9c6c2d304e81c4291355aa34a23656c82e
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-05-27 23:21:24 +0530

    datatype conversion

---
+++ 01-Basics/03-conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33";
+
+console.log(typeof score);
+console.log(typeof (score))
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" -> 33
+// "33abc" -> NaN
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(typeof isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 -> true
+// 0 -> false
+// "abc" -> true
+// "" -> false
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(typeof someNumber);
+console.log(typeof stringNumber);
+
