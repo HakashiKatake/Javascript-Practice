f272354a4929babbd346fe4894fec70f7835aab7
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-05-29 21:35:36 +0530

    math and random

---
+++ 05-Number/01-number.js
@@ -0,0 +1,19 @@
+const score = 400;
+
+const balance = new Number(100);
+
+console.log(balance);
+
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2)); // 100.00
+
+
+console.log(balance.toPrecision(3)); // 100
+
+const hundreds = 1000000;
+console.log(hundreds.toLocaleString("en-IN")); // 10,00,000
+
+
+
+
---
+++ 05-Number/02-maths.js
@@ -0,0 +1,22 @@
+console.log(Math);
+
+
+console.log(Math.abs(-3));
+console.log(Math.abs(3));
+
+console.log(Math.round(3.4));
+console.log(Math.ceil(3.4));
+console.log(Math.floor(3.4));
+console.log(Math.max(1, 2, 3, 4, 5));
+console.log(Math.min(1, 2, 3, 4, 5));
+
+console.log(Math.random()); // random number between 0 and 1
+console.log((Math.random() * 10) + 1); // random number between 0 and 10
+
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min); // random number between 10 and 20
+
+
