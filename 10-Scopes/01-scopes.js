930fd677e0bd11ccb46c8b9ca9dd861bcf8f3242
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-05-30 13:46:25 +0530

    scopes

---
+++ 10-Scopes/01-scopes.js
@@ -0,0 +1,16 @@
+let a = 10;
+const b  = 20; //global scope
+//var c = 30;
+
+if (true) {
+    let a = 210; //blocked scope
+    const b = 20;
+    console.log("INNER: ", a)
+   
+}
+
+console.log(a);
+console.log(b);
+
+
+
