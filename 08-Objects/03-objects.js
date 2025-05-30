76ce6003930327459d8799e042deebc1fc53a40d
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-05-30 11:31:58 +0530

    objs-p3

---
+++ 08-Objects/03-objects.js
@@ -0,0 +1,18 @@
+const course = {
+    title: "JavaScript Basics",
+    price: 29.99,
+    courseInstructor: "John Doe",
+}
+
+const {courseInstructor: instructor} = course;
+
+
+console.log(instructor); // John Doe
+
+
+// {
+//     name: "Saurabh",
+//     coursename: "JavaScript Basics",
+//     price: "free"
+//  }
+
