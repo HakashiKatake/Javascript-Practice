46f063f7d57053da6d2121b75616c81861cc4060
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-05-30 14:12:50 +0530

    IIFE

---
+++ 12-IIFE/01-iife.js
@@ -0,0 +1,18 @@
+//immediately invoked function expression
+
+(function yo() {
+  console.log("I am an IIFE");
+})();
+
+
+( () => {
+    console.log("I am an IIFE with arrow function syntax");
+    
+})();
+
+// IIFE with parameters
+
+(function (name) {
+    console.log(`Hello, ${name}! I am an IIFE with parameters.`);
+}
+)("Saurabh");
